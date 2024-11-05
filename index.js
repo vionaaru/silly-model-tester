583b0e0285c8d44713c18b1f501a6a634753d602
Vionaaru <d.s.kasimov@gmail.com>
2024-11-05 17:36:57 +0300

    Добавлено обновление манифеста, новый index.js и пустой style.css для SillyModelTester

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
+// Подключаем необходимые функции из SillyTavern
 import { getContext } from "../../extensions.js";
 import { eventSource, event_types } from "../../../script.js";
 
-// Получаем контекст SillyTavern
+// Получаем контекст приложения SillyTavern
 const context = getContext();
+
+// Лог для хранения сообщений и ответов
 const log = [];
 
 // Функция для отправки тестового сообщения и получения ответа
 async function sendTestMessage(message) {
+    // Используем generateMessage из контекста для отправки сообщения
     const response = await context.chat.generateMessage({ prompt: message });
     log.push({ message, response });
     console.log(`Сообщение: ${message}\nОтвет: ${response}\n\n`);
 }
 
-// Команда /test для начала тестирования
+// Команда /test для запуска тестирования
 function handleTestCommand() {
     const testMessages = [
         "Привет!",
@@ -26,17 +30,18 @@ function handleTestCommand() {
     });
 }
 
-// Слушаем события
+// Слушаем событие получения сообщения
 eventSource.on(event_types.MESSAGE_RECEIVED, (data) => {
     console.log("Сообщение получено:", data.message);
 });
 
+// Слушаем событие отправки сообщения
 eventSource.on(event_types.MESSAGE_SENT, (data) => {
     console.log("Сообщение отправлено:", data.message);
 });
 
 // Регистрируем команду /test
-import { SlashCommandParser } from "../../slash-commands.js";
+import { SlashCommandParser, SlashCommand } from "../../slash-commands.js";
 SlashCommandParser.addCommandObject(SlashCommand.fromProps({
     name: "test",
     callback: handleTestCommand,
